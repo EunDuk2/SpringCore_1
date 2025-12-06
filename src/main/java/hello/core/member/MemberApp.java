a7829763b46cce5043698d8409d03c6ba4ae5717
EunDuk2 <dmsqls6789@naver.com>
2025-12-07 02:56:32 +0900

    SpringContainer 도입

--- src/main/java/hello/core/member/MemberApp.java
+++ src/main/java/hello/core/member/MemberApp.java
@@ -1,13 +1,16 @@
 package hello.core.member;
 
 import hello.core.AppConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+        // 얘가 스프링 컨테이너라고 봐도 된다
+        // AppConfig 안에 있는 Bean 객체들을 스프링 컨테이너에 넣어 관리한다는 뜻
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);// 이름은 메서드 이름
 
-//        MemberService memberService = new MemberServiceImpl();
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
