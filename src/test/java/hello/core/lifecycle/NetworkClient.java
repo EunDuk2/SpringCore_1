fec90a767f0bde79b384f0b233cda138d1f0441e
EunDuk2 <dmsqls6789@naver.com>
2025-12-12 03:18:14 +0900

    빈 콜백 -> 어노테이션 방식

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,5 +1,8 @@
 package hello.core.lifecycle;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 public class NetworkClient {
 
     private String url;
@@ -26,6 +29,7 @@ public class NetworkClient {
         System.out.println("close " + url);
     }
 
+    @PostConstruct
     public void init() {
         // 의존관계 주입이 끝나면 호출
         System.out.println("NetworkClient.init");
@@ -33,6 +37,7 @@ public class NetworkClient {
         call("초기화 연결 메시지");
     }
 
+    @PreDestroy
     public void close() {
         // 소멸 직전 호출
         System.out.println("NetworkClient.close");
